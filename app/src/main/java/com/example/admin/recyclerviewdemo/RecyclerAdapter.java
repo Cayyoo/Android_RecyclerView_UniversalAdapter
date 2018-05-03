6ee8e9c4ec40ca0512c1a84cbbce9646051a6d54
Kay You <ykmeory@163.com>
2018-05-03 13:47:14 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/admin/recyclerviewdemo/RecyclerAdapter.java
@@ -0,0 +1,75 @@
+package com.example.admin.recyclerviewdemo;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.List;
+
+/**
+ * 万能适配器
+ */
+public abstract class RecyclerAdapter<T> extends RecyclerView.Adapter<RecyclerHolder> {
+
+    private Context mContext;
+    private List<T> mDatas;
+    private int mLayoutId;
+    private LayoutInflater mInflater;
+
+    private OnItemClickListener onItemClickListener;
+
+    public RecyclerAdapter(Context mContext, List<T> mDatas, int mLayoutId) {
+        this.mContext = mContext;
+        this.mDatas = mDatas;
+        this.mLayoutId = mLayoutId;
+        mInflater = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public RecyclerHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new RecyclerHolder(mInflater.inflate(mLayoutId, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(final RecyclerHolder holder, int position) {
+        convert(holder, mDatas.get(position), position);
+
+        if (onItemClickListener != null) {
+            Log.i("TAG","点击");
+
+            //设置背景
+            holder.itemView.setBackgroundResource(R.drawable.recycler_bg);
+
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //注意，这里的position不要用上面参数中的position，会出现位置错乱
+                    onItemClickListener.OnItemClickListener(holder.itemView, holder.getLayoutPosition());
+
+                    Toast.makeText(mContext,"android 21(5.0) ripple水波纹效果",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+    }
+
+    public abstract void convert(RecyclerHolder holder, T data, int position);
+
+    @Override
+    public int getItemCount() {
+        return mDatas!=null ? mDatas.size():0;
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    interface OnItemClickListener {
+        void OnItemClickListener(View view, int position);
+    }
+
+}
